d5a5a40db81cf8ea3dfe355be822165f8b29e12e
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-19 15:21:23 -0400

    Get by SKU

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -30,5 +30,62 @@ namespace Inventory_Api.Controllers
       return info;
     }
 
+    [HttpGet("{Id}")]
+
+    public ActionResult<StoreItem> GetOneItem(int Id)
+    {
+      var db = new DatabaseContext();
+      var getOne = db.Item.FirstOrDefault(f => f.Id == Id);
+      return getOne;
+    }
+
+    [HttpPut("{Id}")]
+
+    public ActionResult<StoreItem> updateItem(int Id, [FromBody]StoreItem item)
+    {
+      var db = new DatabaseContext();
+      var updateOne = db.Item.FirstOrDefault(f => f.Id == Id);
+      item.Price = item.Price;
+      db.SaveChanges();
+      return item;
+    }
+
+    [HttpDelete("{Id}")]
+
+    public ActionResult deleteItem(int Id)
+    {
+      var db = new DatabaseContext();
+      var deleteItem = db.Item.FirstOrDefault(f => f.Id == Id);
+      if (deleteItem == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        db.Item.Remove(deleteItem);
+        db.SaveChanges();
+        return Ok();
+      }
+    }
+    [HttpGet("OutOfStock")]
+
+    public ActionResult<List<StoreItem>> showOutOfStock()
+    {
+      var db = new DatabaseContext();
+      var outOfStock = db.Item.Where(w => w.NumberInStock == 0);
+      return outOfStock.ToList();
+    }
+
+    [HttpGet("SKU{SKU}")]
+
+    public ActionResult<StoreItem> searchBySKU(int SKU)
+    {
+      var db = new DatabaseContext();
+      var findBySKU = db.Item.FirstOrDefault(f => f.SKU == SKU);
+      return findBySKU;
+
+    }
+
   }
+
 }
